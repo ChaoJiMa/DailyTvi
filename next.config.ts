6996b22be7a64f9593e3266d82c95a3eaffa23cc
HJC <894905469@qq.com>
2025-08-01 18:38:03 +0800

    初始化

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
   distDir: 'docs',
   basePath: "",
   Rewrites: "",
+  assetPrefix: "/DailyTvi/",
   Redirects: "",
   Headers: "",
   generateBuildId: async () => {
