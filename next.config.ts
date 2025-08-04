0bb114a96137e900b2bb33de1c04d77a98134641
HJC <894905469@qq.com>
2025-08-04 13:04:53 +0800

    添加首页

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,17 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   distDir: 'docs',
-  basePath: process.env.NODE_ENV === 'production' ? '/DailyTvi' : '',
+  basePath: process.env.NODE_BASEPATH,
   Rewrites: "",
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/DailyTvi' : '',
+  assetPrefix: process.env.NODE_BASEPATH,
   Redirects: "",
   Headers: "",
+  appDir: true,  // 启用App Router
   generateBuildId: async () => {
     return '894905469';
   },
   env: {
-    NEXT_PUBLIC_ENV: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    NEXT_PUBLIC_ENV: process.env.NODE_ENV,
   },
   // 路由
   // exportPathMap: async function (
